93bea47c475e3b160efa36c6133a9268710f73a8
souta1903 <93124330+souta1903@users.noreply.github.com>
2021-12-01 13:26:26 +0900

    マイグレーションファイルとテーブルを作成

--- database/migrations/2021_11_30_044045_create_restaurants_table.php
+++ database/migrations/2021_11_30_044045_create_restaurants_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateRestaurantsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('restaurants', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->integer('telenumber');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('address');
+            $table->string('lunchhour');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('restaurants');
     }
 }
