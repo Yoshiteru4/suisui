0e445b14155726b8a43a60deb348fb4c806f3913
souta1903 <93124330+souta1903@users.noreply.github.com>
2021-12-03 19:54:01 +0900

    blade.phpの見た目を編集

--- resources/views/payment_check.blade.php
+++ resources/views/payment_check.blade.php
@@ -7,6 +7,8 @@
 @section('top','決済確認')
 @section('content')
 <h1>＜注文詳細確認＞</h1>
+
+<form action="payment_finish.blade.php" method="post">
     <div class="order-check">
         <span>選んだメニュー  </span><span>--個</span><br>
         <span>人数</span><span>--個</span><br>
@@ -15,6 +17,10 @@
     <div class="order-amount">
         <span>合計金額</span><span>--円</span>
     </div>
+    <div class="btn">
+    <input type="submit" value="注文確定">
+</form>
+</div>
 @endsection
 
     
