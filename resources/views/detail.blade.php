735f4c768f303dd6bd1709b81e1f991b84cb2f37
souta1903 <93124330+souta1903@users.noreply.github.com>
2021-12-03 18:08:38 +0900

    blade.phpを編集

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,14 +1,19 @@
 @extends('layouts.form')
 
-@section('title', 'detail')
+@section('title', '詳細確認')
 
 <link rel="stylesheet" href="{{ asset('css/detail.css') }}">
 
 @section('top','詳細選択')
 
 @section('content')
-<h1>ki</h1>
-    
+<h1>＜詳細選択＞</h1>
+
+<div class="order-detail">
+<span>選んだメニュー  </span><span>--個</span><br>
+<span>人数</span><span>--個</span><br>
+<span>来店日時</span><span>ー月 ー 日ー 時 ー 分</span>
+</div>
 @endsection
 
    
--- resources/views/payment_check.blade.php
+++ resources/views/payment_check.blade.php
@@ -1,11 +1,20 @@
 @extends('layouts.form')
 
-@section('title', 'detail')
+@section('title', '決済確認')
 
+<link rel="stylesheet" href="{{ asset('css/payment_check.css') }}">
+
+@section('top','決済確認')
 @section('content')
-<h1>決済確認</h1>
-<h2>＜注文詳細確認＞</h2>
-    
+<h1>＜注文詳細確認＞</h1>
+    <div class="order-check">
+        <span>選んだメニュー  </span><span>--個</span><br>
+        <span>人数</span><span>--個</span><br>
+        <span>来店日時</span><span>ー月 ー 日ー 時 ー 分</span>
+    </div>
+    <div class="order-amount">
+        <span>合計金額</span><span>--円</span>
+    </div>
 @endsection
 
     
