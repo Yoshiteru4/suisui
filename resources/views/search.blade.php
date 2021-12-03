30e84cf4fa09271df9ebefaedb5f536be4e07fb6
Yoshiteru4 <93124375+Yoshiteru4@users.noreply.github.com>
2021-12-03 19:05:02 +0900

    a

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,10 +1,16 @@
-@extends('layouts.form')
+   @extends('layouts.form')
 
-@section('title', '検索')
+   @section('title', '検索')
 
 
-@section('top','検索')
+   @section('top','検索')
 
-@section('content')
 
-@endsection
+   @section('content')
+      <form action="soshinsaki.php" method="post">
+        
+         <input type="text" name="input1">
+        
+         <input type="submit" name="submit" value="検索">
+      </form>
+   @endsection
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,10 +1,26 @@
-@extends('layouts.form')
+   @extends('layouts.form')
 
-@section('title', 'ここに入れたいタイトルを書く')
+   @section('title', 'ここに入れたいタイトルを書く')
 
-@section('top','アカウント')
+   @section('top','アカウント')
 
 
-@section('content')
-
-@endsection
+   @section('content')
+   <!DOCTYPE html>
+   <html lang="ja">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Document</title>
+      <link rel="stylesheet" href="{{ asset('css/user.css') }}">
+   </head>
+   <body>
+      <div class="user">
+         <h2>基本情報</h2>
+         <h2>お気に入り一覧</h2>
+         <h2>決済方法</h2>
+      </div>
+   </body>
+   </html>
+   @endsection
