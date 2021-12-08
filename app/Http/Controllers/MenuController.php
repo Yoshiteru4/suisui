c0ee3e9f78df83d1a80acf7f1d2c022a91c022d0
Yoshiteru4 <93124375+Yoshiteru4@users.noreply.github.com>
2021-12-08 20:30:59 +0900

    ee

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -10,75 +10,28 @@ use Illuminate\Support\Facades\Auth;
 class MenuController extends Controller
 {
     //
-    public function MenuPage()
+    public function indexd()
     {
-        return view('menu_list');
+      $menus = Menu::all();
+      return view('menu_list',['menus='=>$menus]);
+    }
+    public function create()
+    {
+      return view('create');
     }
 
-    public function DetailPage()
+    public function store(Request $request)
     {
-
-        return view('detail');
+        $menus = new Menu();
+        $menus->restaurant_id=1;
+        $menus->food = $request->food;
+        $menus->price = $request->price;
+        $menus->save();
+        return redirect('/menu_list');
     }
-    public function index()
+    public function show()
     {
-        $menus = Menu::all();;
-        return view('index')->with('menus',$menus);
-    } 
-    public function search(Request $request)
-    {
-        $keyword_food = $request->food;
-        $keyword_price = $request->price;
-
-        if(!empty($keyword_food) && empty($keyword_price)) {
-            $query = Menu::query();
-            $menus = $query->where('food','like', '%' .$keyword_food. '%')->get();
-            $message = "「". $keyword_food."」を含む名前の検索が完了しました。";
-            return view('/index')->with([
-              'menus' => $menus,
-              'message' => $message,
-            ]);
-        }
-    //     elseif(empty($keyword_food) && $keyword_price >= 0 ){
-    //         $message = "金額の条件を選択してください";
-    //         return view('/index')->with([
-    //           'message' => $message,
-    //         ]);
-    //   }
-        elseif(empty($keyword_food) && $keyword_price ){
-        $query = Menu::query();
-        $menus = $query->where('price','>=', $keyword_price)->get();
-        $message = $keyword_price. "円以上の検索が完了しました";
-        return view('/index')->with([
-          'menus' => $menus,
-          'message' => $message,
-        ]);
-      }
-      elseif(empty($keyword_food) && $keyword_price){
-        $query = Menu::query();
-        $menus = $query->where('price','<=', $keyword_price)->get();
-        $message = $keyword_price. "円以下の検索が完了しました";
-        return view('/index')->with([
-          'menus' => $menus,
-          'message' => $message,
-        ]);
-      }
-
-
-      elseif(!empty($keyword_food) && $keyword_price){
-        $query = Menu::query();
-        $menus = $query->where('food','like', '%' .$keyword_food. '%')->where('price','>=', $keyword_price)->get();
-        $message = "「".$keyword_food . "」を含む名前と". $keyword_price. "円以上の検索が完了しました";
-        return view('/index')->with([
-          'menus' => $menus,
-          'message' => $message,
-        ]);
-      }
-      else {
-        $message = "検索結果はありません。";
-        return view('/index')->with([
-            'message'=>$message,
-        ]);
-      }
+      $menus = Menu::all();
+      return view('/menu_list', ['menus'=>$menus]);
     }
 }
