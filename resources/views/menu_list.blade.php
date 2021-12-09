89a7d429b1c43f82a9ee54948810aa554e200d05
Yoshiteru4 <93124375+Yoshiteru4@users.noreply.github.com>
2021-12-09 18:31:43 +0900

    img 追加

--- resources/views/menu_list.blade.php
+++ resources/views/menu_list.blade.php
@@ -2,12 +2,16 @@
 
 @section('title', 'detail')
 
+<link rel="stylesheet" href="{{ asset('css/menu.css') }}">
+
 @section('top','メニューリスト')
 
 @section('content')
 @foreach($menus as $menu)
-   <p>{{ $menu->food }}</p>
-   <p>{{ $menu->price }}</p>
-   <hr>
+   <div class="menubox">
+      <img src="{{ $menu->image_path }}" alt="画像">
+      <p>{{ $menu->food }}</p>
+      <p>{{ $menu->price }}¥</p>
+   </div>
 @endforeach
 @endsection
