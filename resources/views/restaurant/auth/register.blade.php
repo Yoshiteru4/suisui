c6695a4a65f74ff2160239792a0e78124c6e9f38
souta1903 <93124330+souta1903@users.noreply.github.com>
2022-01-03 08:26:38 +0900

    Update register.blade.php

--- resources/views/restaurant/auth/register.blade.php
+++ resources/views/restaurant/auth/register.blade.php
@@ -15,7 +15,7 @@
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('お名前') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="すいすい太郎" size="35">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="すいすいレストラン" size="35">
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
